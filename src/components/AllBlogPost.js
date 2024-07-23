622654cc75fa7f6b9fe0c01facf058f588d3907e
ksubi666 <ksubi666@gmail.com>
2024-07-24 01:01:40 +0800

    +post filter

--- src/components/AllBlogPost.js
+++ src/components/AllBlogPost.js
@@ -2,20 +2,22 @@ import { Link } from 'react-router-dom';
 import { AllBlogContent, AllBlogPostTag } from '../constants.js/index.js';
 import { LoadMore } from './LoadMore.js';
 import { Post } from './Post.js';
+import { useState } from 'react';
 const styles = {
   conatiner: 'flex flex-col items-center gap-8',
   textContainer:
     'flex flex-col w-[1216px] gap-8 text-[#495057] font-bold self-strech',
   header: 'font-bold text-2xl leading-7 text-[#181A2A]',
-  tag: 'text-[#495057] text-xs font-bold leading-6 hover:text-[#D4A373]',
+  tag: 'text-[#495057] text-sm font-bold leading-[25px] hover:text-[#D4A373] active:text-[#D4A373]',
   postContainer: 'flex flex-wrap w-[1216px] gap-5',
   button:
     'py-3 px-5 items-center rounded-[6px] border-[#696A754D] border-[1px] mb-[80px]',
 };
 
 export const AllBlogPost = ({ postTag }) => {
-  const Click = () => {
-    console.log('kk');
+  const [allPost, setAllPost] = useState(AllBlogContent);
+  const Click = (i) => {
+    setAllPost(allPost.filter((el) => el.tag === AllBlogPostTag[i]));
   };
 
   return (
@@ -23,10 +25,10 @@ export const AllBlogPost = ({ postTag }) => {
       <div className={styles.textContainer}>
         <h1 className={styles.header}>All Blog Post</h1>
         {postTag ? (
-          <div className="flex cursor-pointer justify-between items-center text-center">
+          <div className="flex cursor-pointer justify-between items-center text-center text-">
             <div className="flex  gap-5 ">
-              {postTag.map((el) => (
-                <p className={styles.tag} onClick={Click}>
+              {postTag.map((el, i) => (
+                <p className={styles.tag} onClick={() => Click(i)}>
                   {el}
                 </p>
               ))}
@@ -39,7 +41,7 @@ export const AllBlogPost = ({ postTag }) => {
       </div>
       {postTag ? (
         <div className={styles.postContainer}>
-          {AllBlogContent.map((el, index) => (
+          {allPost.map((el, index) => (
             <Post
               key={index}
               img={el.img}
