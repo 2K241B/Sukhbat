35c02fb8942d963a447e4071c6b55cdd37f32bcc
ksubi666 <ksubi666@gmail.com>
2024-07-26 16:53:15 +0800

    fix

--- src/Pages/BlogPost.js
+++ src/Pages/BlogPost.js
@@ -13,19 +13,26 @@ export const BlogPost = () => {
     return { __html: data.body_html };
   }
   return (
-    <div className="w-[1216px] justify-center">
+    <div className="w-[1216px] flex flex-col justify-center gap-8">
       <h2 className="text-[36px] font-semibold">{data.title}</h2>
-      <div className="flex items-center">
+      <div className="flex items-center gap-2">
         <img
           className="size-[28px] rounded-full"
           src={data?.user?.profile_image}
           alt="ProfileImage"
         ></img>
-        <p>{data?.user?.username}</p>
-        <p>{data?.published_at}</p>
+        <p className="text-[14px] leading-5 text-[#696A75] font-medium capitalize">
+          {data?.user?.username}
+        </p>
+        <p className="text-[14px] leading-5 text-[#696A75] ml-3">
+          {data?.published_at}
+        </p>
       </div>
       <img src={data?.social_image} alt="coverImage" />
-      <div dangerouslySetInnerHTML={createMarkup()} />
+      <div
+        className="flex flex-col gap-5 text-xl leading-8 text-[#3B3C4A]"
+        dangerouslySetInnerHTML={createMarkup()}
+      />
     </div>
   );
 };
