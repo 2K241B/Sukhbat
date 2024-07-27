b37f2eb277c45fb227709df6aa9ef8a53bf8aad4
ksubi666 <ksubi666@gmail.com>
2024-07-27 21:22:04 +0800

    GG

--- src/Pages/BlogPost.js
+++ src/Pages/BlogPost.js
@@ -4,8 +4,8 @@ import { useParams } from 'react-router-dom';
 const styles = {
   container: 'w-[1216px] flex flex-col justify-center gap-8',
   title: 'text-[36px] font-semibold',
-  username: 'text-[14px] leading-5 text-[#696A75] font-medium capitalize',
-  date: 'text-[14px] leading-5 text-[#696A75] ml-3',
+  username: 'text-base font-medium leading-6 text-[#97989F] capitalize',
+  date: 'text-[#97989F] text-[14px] font-normal leading-6',
   article: 'flex flex-col gap-5 text-xl leading-8 text-[#3B3C4A]',
 };
 export const BlogPost = () => {
@@ -22,13 +22,15 @@ export const BlogPost = () => {
   return (
     <div className={styles.container}>
       <h2 className={styles.title}>{data.title}</h2>
-      <div className="flex items-center gap-2">
-        <img
-          className="size-[28px] rounded-full"
-          src={data?.user?.profile_image}
-          alt="ProfileImage"
-        ></img>
-        <p className={styles.username}>{data?.user?.username}</p>
+      <div className="flex items-center gap-5">
+        <div className="flex items-center gap-3">
+          <img
+            className="size-[28px] rounded-full"
+            src={data?.user?.profile_image}
+            alt="ProfileImage"
+          ></img>
+          <p className={styles.username}>{data?.user?.name}</p>
+        </div>
         <p className={styles.date}>{data?.published_at}</p>
       </div>
       <img src={data?.social_image} alt="coverImage" />
