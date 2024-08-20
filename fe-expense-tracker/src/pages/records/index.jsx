cfb000040b8343d56b755b3e4ca04f9d656a0d95
Ksubi666 <Ksubi666@gmail.com>
2024-08-20 23:18:04 +0800

    .

--- fe-expense-tracker/src/pages/records/index.jsx
+++ fe-expense-tracker/src/pages/records/index.jsx
@@ -1,12 +1,27 @@
 import CategoryMenu from '@/components/CategoryMenu';
 import Layout from '@/components/Layout';
 import RecordsListTable from '@/components/RecordsListTable';
+import axios from 'axios';
+import { useEffect, useState } from 'react';
 
 const Records = () => {
+  const [recordData, setRecordData] = useState();
+  const [currency, setCurrency] = useState('MNT');
+  useEffect(() => {
+    let user = localStorage.getItem('user');
+    const data = JSON.parse(user);
+    const userId = data.user.id;
+    const currenryType = data.user.currency_type;
+    setCurrency(currenryType);
+    axios
+      .get(`http://localhost:8000/record/id/${userId}`)
+      .then((res) => setRecordData(res.data));
+    console.log(recordData);
+  }, []);
   return (
     <Layout ChildStyle={true}>
       <CategoryMenu />
-      <RecordsListTable />
+      <RecordsListTable recordData={recordData} currency={currency} />
     </Layout>
   );
 };
